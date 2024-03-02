865f6e90ccf26cc08eff35935e92a997821d7c8f
mohammedchakir <zerroukimedchakir@gmail.com>
2024-03-02 02:42:42 +0100

    add

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,11 @@
-$(document).ready(function() {
-    // Listen for changes on each input checkbox tag
-    $('input[type="checkbox"]').change(function() {
-      var checkedAmenities = [];
-      
-      // If the checkbox is checked, store the Amenity ID in a variable
-      $('input[type="checkbox"]:checked').each(function() {
-        checkedAmenities.push($(this).data('id'));
-      });
-      
-      // Update the <h4> tag inside the div Amenities with the list of Amenities checked
-      $('.amenities h4').text(checkedAmenities.join(', '));
-    });
+(document).ready(function () {
+  const amenities = {};
+  $('li input[type=checkbox]').change(function () {
+    if (this.checked) {
+      amenities[this.dataset.name] = this.dataset.id;
+    } else {
+			  delete amenities[this.dataset.name];
+    }
+    $('.amenities h4').text(Object.keys(amenities).sort().join('', ''));
   });
-  
+});
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,11 +1,19 @@
-$(document).ready(function() {
-    // Request http://0.0.0.0:5001/api/v1/status/
-    $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
-      if (data.status === 'OK') {
-        $('#api_status').addClass('available');
-      } else {
-        $('#api_status').removeClass('available');
-      }
-    });
+(document).ready(function () {
+  const amenities = {};
+  $('li input[type=checkbox]').change(function () {
+    if (this.checked) {
+      amenities[this.dataset.name] = this.dataset.id;
+    } else {
+			  delete amenities[this.dataset.name];
+    }
+    $('.amenities h4').text(Object.keys(amenities).sort().join('', ''));
   });
-  
+});
+// Request http://0.0.0.0:5001/api/v1/status/
+$.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
+  if (data.status === 'OK') {
+    $('div#api_status').addClass('available');
+  } else {
+    $('div#api_status').removeClass('available');
+  }
+});
